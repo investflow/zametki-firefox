afa7b8e671d6ea755c8521e6ab0d66c34a216bad
Mikhail Fursov <fursovm@amazon.com>
2016-08-29 12:44:32 -0700

    Adding experimental support for android browser

--- background.js
+++ background.js
@@ -1,16 +1,24 @@
-browser.contextMenus.create({
-    title: "Zametka!",
-    contexts: ["page", "selection", "link"],
-    onclick: process
-});
+var title = "-> Investflow";
+if (window.NativeWindow) { // Android browser
+    var selector = window.NativeWindow.contextmenus.SelectorContext("*");
+    window.NativeWindow.contextmenus.add(title, selector, function () {
+        process({})
+    });
+} else { // Desktop browser
+    chrome.contextMenus.create({
+        title: title,
+        contexts: ["page", "selection", "link"],
+        onclick: process
+    });
+}
 
 function process(info) {
-    browser.tabs.query({active: true}, function (tabs) {
+    chrome.tabs.query({active: true}, function (tabs) {
         var tab = tabs[0];
         var title = tab.title;
         var text = info.selectionText ? info.selectionText : "";
         var url = info.linkUrl ? info.linkUrl : info.srcUrl ? info.srcUrl : info.pageUrl ? info.pageUrl : info.frameUrl ? info.frameUrl : tab.url;
-        browser.tabs.create({"url": "http://investflow.ru/zametki/new?title=" + title + "&text=" + text + "&url=" + url});
+        chrome.tabs.create({"url": "http://investflow.ru/zametki/new?title=" + title + "&text=" + text + "&url=" + url});
     });
 }
 
